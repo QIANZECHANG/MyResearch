71dd62f05928aef6705fb0214c8095d5f7392ea8
QIANZECHANG <qianzechang@gmail.com>
2021-10-16 01:31:12 +0900

    2021-10-16

--- current/test1.c
+++ current/test1.c
@@ -1,13 +1,15 @@
-void func()
-{ 
-    int *p=malloc(1);
-    int* q;
-    q=malloc(sizeof(int));
-    char* r;
-    r=(char*)malloc(sizeof(char));
-    float* f;
-    double* d;
-    unsigned int* ui=malloc(1);
+struct M{int* p1; int* p2;};
+void func(int a, int b){
+    struct M m;
+    m.p1=(int*)malloc(4);
+    m.p2=(int*)malloc(4);
+    int c=a+b;
+    int* n;
+    if(c>5)
+        n=m.p1;
+    else
+        n=malloc(4);
+    free(*(&m.p1 + 1));
+    free(n);
 }
-
 
